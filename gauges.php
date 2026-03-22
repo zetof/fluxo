6d77ecb647d22389c0d014df7ab5158a4841b92e
ZeToF! <zetof@zetof.net>
2026-03-22 18:02:05 +0100

    Add centering, enhance i18n and add title with data freshness indicator

--- gauges.php
+++ gauges.php
@@ -5,6 +5,17 @@
   <script type="text/javascript" src="core/gauge.js"></script>
 </head>
 <body>
+  <div id="datetime" class="wrapper header">
+    <h1>
+      <?php
+      $valid = $data['valid']?'ok':'ko';
+      echo _t('gauges', ['measures'])._d($data['values'][0]['time']).' <span class="'.$valid.'">&#128959;</span>';
+      ?>
+    </h1>
+  </div>
+  <div class="wrapper">
+    <div id="gauges" class="centered"></div>
+  </div>
   <script>
     // Definition of gradients
     gp = ["black", "black", "lightblue", "deepskyblue", "mediumblue"];
@@ -21,8 +32,8 @@
     ?>
     
     // Display 3 gauges for outside pressure, humidity and temperature
-    pression = new Gauge({x:20, y:20, size:300, min:913, max:1113, val:p_now, old:p_old, gradient:gp, caption:"PRESSION", unit:"hPa"});
-    outsideHumidity = new Gauge({x:340, y:20, size:300, min:0, max:100, val:h_now, old:h_old, gradient:gh, caption:"HUMIDITE", unit:"%"});
-    outsideTemp = new Gauge({x:660, y:20, size:300, min:-20, max:50, val:t_now, old:t_old, decimal:true, gradient:gt, caption:"TEMPERATURE", unit:"°C"});
+    pression = new Gauge({container:"gauges", x:20, y:20, size:300, min:913, max:1113, val:p_now, old:p_old, gradient:gp, caption:"<?php _t('gauges',['pressure'], display:true, up:true); ?>", unit:"hPa"});
+    outsideHumidity = new Gauge({container:"gauges", x:340, y:20, size:300, min:0, max:100, val:h_now, old:h_old, gradient:gh, caption:"<?php _t('gauges',['humidity'], display:true, up:true); ?>", unit:"%"});
+    outsideTemp = new Gauge({container:"gauges", x:660, y:20, size:300, min:-20, max:50, val:t_now, old:t_old, decimal:true, gradient:gt, caption:"<?php _t('gauges',['temperature'], display:true, up:true); ?>", unit:"°C"});
   </script>
 </body>
--- i18n.php
+++ i18n.php
@@ -8,12 +8,25 @@ if (!file_exists($langPath)) {
 
 $i18n = parse_ini_file($langPath, true);
 
-function _t($section, array $prompts) {
+function _t($section, array $prompts, $display=false, $up=false, $down=false) {
     global $i18n;
     $constructed = '';
     foreach($prompts as $prompt) {
         if(isset($i18n[$section][$prompt])) $constructed .= $i18n[$section][$prompt];
         else $constructed .= "__i18n__<{$section}><{$prompt}>";
     }
-    return $constructed;
+    if($up) $constructed = mb_strtoupper($constructed, 'UTF-8');
+    if($down) $constructed = strtolower($constructed);
+    if($display) echo $constructed;
+    else return $constructed;
+}
+
+function _d($datetime, $display=false) {
+    global $i18n;
+    $timeStamp = date_create($datetime);
+    $formatedDate = date_format($timeStamp, $i18n['datetime']['format1']);
+    $formatedDate .= $i18n['datetime'][strtolower(date_format($timeStamp, $i18n['datetime']['format2']))];
+    $formatedDate .= date_format($timeStamp, $i18n['datetime']['format3']);
+    if($display) echo $formatedDate;
+    else return $formatedDate;
 }
